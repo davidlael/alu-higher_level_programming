844b49aded00da2fcc77a9b036056e8ad2208c65
davidlael <d.nziza1@alustudent.com>
2026-07-21 04:12:16 -0700

    Fix syntax for task 0 and task 1 comments and grants

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- Creates the MySQL server user user_0d_1
+-- Script that creates the MySQL server user user_0d_1
+-- Creates user if not exists and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
