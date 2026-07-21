5ce44424e7292087a825be408534c67efa2d0120
davidlael <d.nziza1@alustudent.com>
2026-07-21 04:17:03 -0700

    Add SQL more queries tasks 0 and 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Script that creates the MySQL server user user_0d_1
--- Creates user if not exists and grants all privileges
+-- Create user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
