19fac2bf58e55166ad6722e5115015d0451be1ed
davidlael <d.nziza1@alustudent.com>
2026-07-21 04:05:28 -0700

    Fix Task 1 user creation syntax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- Creates the MySQL server user user_0d_1
+-- Script that creates the MySQL server user user_0d_1
+-- Creates user_0d_1 with all privileges and sets password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
