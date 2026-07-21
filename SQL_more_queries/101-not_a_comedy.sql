9449e4be738580b5b7e386ba23e20c19e61f59d6
davidlael <d.nziza1@alustudent.com>
2026-07-21 04:01:13 -0700

    Add advanced SQL query tasks 17 through 20

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Script that lists all shows without the genre Comedy
+-- Uses a subquery to exclude shows with Comedy genre
+SELECT title FROM tv_shows
+WHERE id NOT IN (
+    SELECT tv_shows.id
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
