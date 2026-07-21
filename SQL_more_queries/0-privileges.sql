6774ea6604ef5b7c317f652003cc941d73104f61
davidlael <d.nziza1@alustudent.com>
2026-07-21 03:51:23 -0700

    Fix task 0 and task 1 privileges scripts

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
--- Displays grants for user_0d_1
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Displays grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Script that creates the MySQL server user user_0d_1
--- Creates user_0d_1 with all privileges and password user_0d_1_pwd if not exists
+-- Creates the MySQL server user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grants all privileges on all databases to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
