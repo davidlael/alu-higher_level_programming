d6cfa5a2fa5699232219cc1fc590ae8a12642c5f
davidlael <d.nziza1@alustudent.com>
2026-07-25 01:21:21 -0700

    Fix header script for task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header variable
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+# Sends a GET request with a header X-HolbertonSchool-User-Id: 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
