caf5e63d1b7441eeb18a30f120465bea386a73ac
davidlael <d.nziza1@alustudent.com>
2026-07-25 02:36:11 -0700

    Fix lazy matrix mul incompatible dimensions test

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Module for matrix multiplication using NumPy."""
+"""Module for lazy matrix multiplication using NumPy"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies 2 matrices using NumPy.
-
-    Args:
-        m_a: First matrix.
-        m_b: Second matrix.
-
-    Returns:
-        Product of m_a and m_b using numpy.matmul.
-    """
-    return np.matmul(m_a, m_b)
+    """Multiplies two matrices using NumPy"""
+    try:
+        return np.matmul(m_a, m_b).tolist()
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
