8ed671185db827f6cf06a0636beea765e71714c7
davidlael <d.nziza1@alustudent.com>
2026-07-25 02:45:42 -0700

    Fix lazy matrix mul with exact numpy error messages

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -13,7 +13,4 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         Result of matrix multiplication as numpy array
     """
-    try:
-        return np.matmul(m_a, m_b)
-    except ValueError:
-        raise ValueError("m_a and m_b can't be multiplied")
+    return np.matmul(m_a, m_b)
