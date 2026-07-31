6fc732caf86e2eb3919a347fa137dbbeef6f52ff
davidlael <d.nziza1@alustudent.com>
2026-07-31 00:37:54 -0700

    Add 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with X-HolbertonSchool-User-Id header set to 98
+# Sends a GET request to a URL with a header variable X-HolbertonSchool-User-Id: 98
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
