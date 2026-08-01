ace3349058ec6c60d2bb28458a71d110ac30b443
davidlael <d.nziza1@alustudent.com>
2026-08-01 12:29:23 -0700

    Fix 0-privileges.sql and 1-create_user.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
--- on your server (in localhost).
+-- Show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
